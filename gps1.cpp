6426d0999441c102e5623bb20d3a1dcc113fb9d2
suman phuyal <sfuyal59@gmail.com>
2026-01-16 23:37:06 +0545

    add skeleton structore

---
+++ gps1.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+using namespace std;
+
+float gps_to_position(float lat, float lon){
+    
+}
+
+
+float kalman_filter(float A, float B, float C){
+    // implements the linear kalman filter algorithm
+}
+
+int main(){
+
+    return 0;
+}
